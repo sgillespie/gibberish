08c33a7412d72e950944be3ef0457c1aca96cbb4
Sean Gillespie <sean@mistersg.net>
2015-05-07 22:56:12 -0400

    Fix compiler errors

--- src/cli/Main.hs
+++ src/cli/Main.hs
@@ -1,6 +1,9 @@
 module Main where
 
+import Control.Monad
+import System.Random
+
 import Elocrypt.Password
 
 main :: IO ()
-main = putStrLn (generate getStdGen)
+main = (fst . generate) `liftM` getStdGen >>= putStrLn
