063e97689f210099b1c34bb57a47a31e8f47ac14
Sean Gillespie <sean@mistersg.net>
2015-05-08 20:46:31 -0400

    Add a length parameter to password generator

--- src/lib/Elocrypt/Password.hs
+++ src/lib/Elocrypt/Password.hs
@@ -3,10 +3,10 @@ module Elocrypt.Password where
 import Control.Monad.Random
 import System.Random
 
-generate :: RandomGen g => g -> (String, g)
-generate gen = runRand m gen
+generate :: RandomGen g => g -> Int -> (String, g)
+generate gen len = runRand m gen
   where weights = zip alphabet (repeat 1)
-        m = sequence . take 6 . repeat . fromList $ weights
+        m = sequence . take len . repeat . fromList $ weights
 
 alphabet :: [Char]
 alphabet = ['a'..'z']
--- test/Elocrypt/PasswordTest.hs
+++ test/Elocrypt/PasswordTest.hs
@@ -3,7 +3,7 @@ module Elocrypt.PasswordTest where
 
 import Control.Monad
 import System.Random
-import Test.QuickCheck (Arbitrary, arbitrary)
+import Test.QuickCheck hiding (generate)
 import Test.Tasty.QuickCheck (testProperty)
 import Test.Tasty.TH
 
@@ -17,14 +17,20 @@ instance Show GenPassword where
 instance Eq GenPassword where
   (GenPassword p1) == (GenPassword p2) = fst p1 == fst p2
 
-instance Arbitrary GenPassword where
-  arbitrary = (GenPassword . generate . mkStdGen) `liftM` arbitrary
+instance Arbitrary StdGen where
+  arbitrary = mkStdGen `liftM` arbitrary
 
+instance Arbitrary GenPassword where
+  arbitrary = do
+    gen <- arbitrary
+    len <- suchThat arbitrary (>=6)
+    return . GenPassword . generate gen $ len
+ 
 tests = $(testGroupGenerator)
 
 prop_generateShouldBeUnique :: GenPassword -> Bool
 prop_generateShouldBeUnique (GenPassword (p, g))
-  = p /= fst (generate g)
+  = p /= fst (generate g (length p))
 
 prop_generateShouldBeLong :: GenPassword -> Bool
 prop_generateShouldBeLong (GenPassword p) = length (fst p) >= 6
