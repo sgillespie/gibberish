933a0ca55aaac9b6bdf73d3b60cf83e3e3c2c0dc
Sean Gillespie <sean@mistersg.net>
2015-05-11 21:19:14 -0400

    Add a lastN test

--- test/Elocrypt/PasswordTest.hs
+++ test/Elocrypt/PasswordTest.hs
@@ -11,7 +11,7 @@ import Test.Tasty.TH
 
 import Elocrypt.Password
 import Elocrypt.Trigraph
-import Elocrypt.TrigraphTest
+import Elocrypt.TrigraphTest -- TODO: Extract useful instances into common module
 
 newtype GenPassword = GenPassword (String, StdGen)
 
@@ -52,7 +52,34 @@ prop_first2ShouldHaveLength2 :: StdGen -> Bool
 prop_first2ShouldHaveLength2 g = length (evalRand first2 g) == 2
 
 prop_nextShouldSkip0Weights :: TrigraphChar -> TrigraphChar -> StdGen -> Property
-prop_nextShouldSkip0Weights (T c1) (T c2) gen
-  = not (null frequency) ==> evalRand (next . reverse $ f2) gen `elem` (map fst frequency)
+prop_nextShouldSkip0Weights (T c1) (T c2) gen = isCandidate next' [c1, c2]
+  where next' = evalRand (next . reverse $ [c1, c2]) gen
+
+prop_lastNShouldSkip0Weights :: TrigraphChar -> TrigraphChar -> Int -> StdGen -> Property
+prop_lastNShouldSkip0Weights (T c1) (T c2) len gen
+  = len > 0 ==> lastNShouldSkip0Weights' lastN'
   where f2 = [c1, c2]
-        frequency = filter ((/=0) . snd) . zip ['a'..'z'] . fromJust . findFrequency $ f2
+        lastN' = evalRand (lastN (reverse [c1, c2]) len) gen
+
+lastNShouldSkip0Weights' :: String -> Property
+lastNShouldSkip0Weights' (p:ps:pss:psss) = isCandidate p [pss, ps] .&&.
+                                           lastNShouldSkip0Weights' (ps:pss:psss)
+lastNShouldSkip0Weights' _ = property True
+
+-- Utility functions
+isCandidate :: Char -> String -> Property
+isCandidate c (s:ss:sss) = hasCandidates f2 ==>
+                           isJust $ elem c `liftM` findNextCandidates f2
+  where f2 = [s, ss]
+
+isCandidate _ _ = undefined -- This really shouldn't ever happen
+
+hasCandidates :: String -> Bool
+hasCandidates s = (not . null) `liftM` findNextCandidates s == Just True
+
+findNextCandidates :: String -> Maybe [Char]
+findNextCandidates (c1:c2:_) = map fst `liftM` frequency
+  where f2 = [c1, c2]
+        frequency = (filter ((/=0) . snd) . zip ['a'..'z']) `liftM` findFrequency f2
+
+findNextCandidates _ = Nothing  -- This really shouldn't ever happen
