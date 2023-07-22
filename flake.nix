56b3d2526076aa343285072494cac256f0e278ea
Sean D Gillespie <sean@mistersg.net>
2023-07-22 14:54:54 -0400

    ci: Update nix config in GitHub workflow

--- flake.nix
+++ flake.nix
@@ -52,13 +52,15 @@
 
   nixConfig = {
     trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "sgillespie.cachix.org-1:Zgif/WHW2IzHqbMb1z56cMmV5tLAA+zW9d5iB5w/VU4="
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
     ];
     
     substituters = [
-      "https://sgillespie.cachix.org"
+      "https://cache.nixos.org/"
       "https://cache.iog.io"
+      "https://sgillespie.cachix.org"
     ];
 
     allow-import-from-derivation = "true";
